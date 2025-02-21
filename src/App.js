c4aa53181c98f431e70eb03244cc1c3390c8fdff
Elis Mattosinho <eliscmattosinho@gmail.com>
2025-02-21 20:17:06 -0300

    feat: inserção do board

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Building from "./pages/Building";
+import Boards from "./pages/Boards";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div id="main">
+          <Routes>
+            <Route path="/" element={<Building />} />
+            <Route path="/boards" element={<Boards />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

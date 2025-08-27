5c40a8d1c63179b8867507f957e3b82d4934d021
Elis Mattosinho <99125499+eliscmattosinho@users.noreply.github.com>
2025-08-27 16:18:03 -0300

    feat: creating a loading

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Building from "./pages/Building";
 import Boards from "./pages/Boards";
+import RouteChangeTracker from "./components/RouteChangeTracker";
 
 function App() {
   return (
     <Router basename="/Board-de-Desenvolvimento">
-      <div className="App">
-        <div id="main">
-          <Routes>
-            <Route path="/" element={<Building />} />
-            <Route path="/boards" element={<Boards />} />
-          </Routes>
+      <RouteChangeTracker>
+        <div className="App">
+          <div id="main">
+            <Routes>
+              <Route path="/" element={<Building />} />
+              <Route path="/boards" element={<Boards />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </RouteChangeTracker>
     </Router>
   );
 }

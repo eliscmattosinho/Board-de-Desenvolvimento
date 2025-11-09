68a2fb79516f5bd784bf024d9ac36aea7492fcf6
Elis Mattosinho <99125499+eliscmattosinho@users.noreply.github.com>
2025-11-09 18:28:34 -0300

    refactor(exception-pages): unify NotFound and ScreenBlockage pages into ExceptionPage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-
-import { Link } from "react-router-dom";
-
+import ExceptionPage from "./ExceptionPage";
 import error from "../assets/images/404.svg";
-
-import "./NotFound.css";
 
 export default function NotFound() {
     return (
-        <div id="error-content">
-            <h2 className="error-title">Page Not Found</h2>
-
-            <div className="error-info">
-                <p>Sorry, the page you are looking for does not exist.</p>
-                <Link to="/" className="go-back">Go back home</Link>
-            </div>
-
-            <div className="error-img-container">
-                <img src={error} alt="404 component" />
-            </div>
-        </div>
+        <ExceptionPage
+            title="Page Not Found"
+            messages={["Sorry, the page you are looking for does not exist."]}
+            link={{ to: "/", label: "Go back home" }}
+            image={error}
+        />
     );
 }

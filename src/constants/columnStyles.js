11b36fc9540b0e6c5d1aa636389d21ef5f7fe7a2
Elis Mattosinho <99125499+eliscmattosinho@users.noreply.github.com>
2025-11-12 14:52:56 -0300

    refactor(columns): remove columnStyles constant and adapt components to use styleVars

--- src/constants/columnStyles.js
+++
@@ -1,7 +0,0 @@
-export const columnStyles = {
-  todo: { bg: "#3DD6B3", border: "transparent", color: "#efefef"  },
-  progress: { bg: "#2C7FA3", border: "transparent", color: "#efefef"  },
-  done: { bg: "#2DA44A", border: "transparent", color: "#efefef"  },
-  backlog: { bg: "transparent", border: "#2C7FA3", color: "#2C7FA3" },
-  review: { bg: "transparent", border: "#2DA44A", color: "#2DA44A" }
-};
